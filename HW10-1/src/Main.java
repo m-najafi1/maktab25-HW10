9b9a7ec136cb96ae4887f0790911faa11e3623f0
m-najafi1 <mehran.najafi68@gmail.com>
2019-05-27 15:35:45 -0700

    create method adddata() for insert person to list person return list from person
    Use String Class and Grouping Using Collectors.groupingBy
    
    The second part of the question has been used to count the number of words in each group from the Collector.counting () method

--- HW10-1/src/Main.java
+++ HW10-1/src/Main.java
@@ -6,9 +6,47 @@ public class Main {
 
     public static void main(String[] args) {
 
+        List<Person> personList = adddata();
 
 
+        System.out.println( " Grouping Number : "+ personList.stream().collect(Collectors
+                .groupingBy(
+                        (Person p) ->"\n\n"+ p.getName().length())));
+
+
+        System.out.println("\nGrouping Number Item :   "+personList.stream().collect(Collectors
+                .groupingBy(
+                        (Person p) -> p.getName().length(),Collectors.counting())));
 
 
     }
+
+    // Add Data Person
+    static List<Person> adddata() {
+
+        Person person1 = new Person("meharn");
+        Person person2 = new Person("reza");
+        Person person3 = new Person("mohammad");
+        Person person4 = new Person("ali");
+        Person person5 = new Person("ahmad");
+        Person person6 = new Person("nima");
+        Person person7 = new Person("farzan");
+        Person person8 = new Person("mostafa");
+        Person person9 = new Person("abbas");
+        Person person10 = new Person("arash");
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(person1);
+        personList.add(person2);
+        personList.add(person3);
+        personList.add(person4);
+        personList.add(person5);
+        personList.add(person6);
+        personList.add(person7);
+        personList.add(person8);
+        personList.add(person9);
+        personList.add(person10);
+        return personList;
+    }
+
 }
